f49d33faf0ba16888259e57f88b8b8a18925eb40
Emile <despauxemile@gmail.com>
2025-03-16 11:24:06 -0400

    del from stock

--- entrepot.py
+++ entrepot.py
@@ -3,3 +3,10 @@ class Entrepot:
         self.id = id
         self.stock = stock
         self.position = position
+
+
+    def del_from_stock(self, lego: int, qty: int):
+        map_stock = map(lambda s: s["LegoId"], self.stock)
+
+        idx = list(map_stock).index(lego)
+        self.stock[idx]["Quantity"] -= qty
