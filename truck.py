a1a81c8d60b5726e58a65088184b28e095b948f4
Emile <despauxemile@gmail.com>
2025-03-16 11:10:56 -0400

    truck has stock

--- truck.py
+++ truck.py
@@ -13,3 +13,25 @@ class Truck:
         tri = sorted(dist, key=lambda t: t[1])
 
         return tri[0][0]
+
+    def find_entrepot_has_stock(self, lego: int, qty: int, entrepots: list[Entrepot]) -> list[Entrepot]:
+
+        entre: list[Entrepot] = []
+
+        for e in entrepots:
+            # check if stock
+            map_stock = map(lambda s: s["LegoId"], e.stock)
+            
+            try:
+                idx = list(map_stock).index(lego)
+            except:
+                continue
+
+            stock = e.stock[idx]
+
+            if stock["Quantity"] < qty:
+                continue
+
+            entre.append(e)
+
+        return entre
